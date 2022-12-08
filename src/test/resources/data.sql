94b2ac46a63d45a9cc2ec4b004c67d254897408d
EMMANUEL MACAULAY <emmanuelmcl86@gmail.com>
2022-12-08 02:52:27 +0100

    created data.sql in test for db preloading

---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19b44-7689-11ed-a1eb-0242ac120002', 1, 125, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19e3c-7689-11ed-a1eb-0242ac120002', 2, 250, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19f90-7689-11ed-a1eb-0242ac120002', 3, 375, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b1a0e4-7689-11ed-a1eb-0242ac120002', 4, 500, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19b45-7689-11ed-a1eb-0242ac120002', 1, 125, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19b46-7689-11ed-a1eb-0242ac120002', 2, 250, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19b47-7689-11ed-a1eb-0242ac120002', 3, 375, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19b48-7689-11ed-a1eb-0242ac120002', 4, 500, 100,  0);
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19b49-7689-11ed-a1eb-0242ac120002', 4, 500, 100,  0);;
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('f2b19b50-7689-11ed-a1eb-0242ac120002', 4, 500, 100,  0);
+INSERT INTO medication (name, weight, code, image) VALUES ('Felvin_20mg_Capsules', 125, 'DG-1000-A', 'https://books.google.com/books/content?id=BT89ugEACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE71bgquZSfQtYmOdzHtUNZ1TOewfgKWyuRVg4MiYRerErv54rr4mx3H6-bye5JEyXhk36qwLaNX-F8_Aiz_S8YYAFXErb_2_Yj3h4rEUsgRW3f8JoGF8WfdhFxPcLGG28X7MAt_A');
+INSERT INTO medication (name, weight, code, image) VALUES ('Vitamin_B-Complex', 125, 'DG-1000-B', '');
+INSERT INTO medication (name, weight, code, image) VALUES ('Vitamin_C_1000mg', 125, 'DG-1000-C', '');
+INSERT INTO medication (name, weight, code, image) VALUES ('Flagyl_400mg_Tablet', 125, 'DG-1000-D', 'https://www.assetpharmacy.com/wp-content/uploads/2017/09/Flagyl-400mg-Tablets-Metronidazole-400mg-10-Tablets.jpg');
