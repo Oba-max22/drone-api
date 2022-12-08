bd15cc221334173abff87bec616d157663103be7
EMMANUEL MACAULAY <emmanuelmcl86@gmail.com>
2022-12-08 02:53:58 +0100

    created registerDrone api

--- src/main/java/com/emmanuel/macaulay/dronesapi/controller/DispatchController.java
+++ src/main/java/com/emmanuel/macaulay/dronesapi/controller/DispatchController.java
@@ -1,14 +1,29 @@
 package com.emmanuel.macaulay.dronesapi.controller;
 
+import com.emmanuel.macaulay.dronesapi.model.Drone;
+import com.emmanuel.macaulay.dronesapi.payload.request.RegisterRequest;
+import com.emmanuel.macaulay.dronesapi.payload.response.ApiResponse;
+import com.emmanuel.macaulay.dronesapi.service.DroneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/drone")
+@RestController
 public class DispatchController {
-    /*
-    Functional requirements
-    There is no need for UI;
-    Prevent the drone from being loaded with more weight that it can carry;
-    Prevent the drone from being in LOADING state if the battery level is below 25%;
-    Introduce a periodic task to check drones battery levels and create history/audit event log for this.
-    */
-    // TODO: registering a drone;
+    private static final String DRONE_REGISTER_SUCCESS = "Drone registered successfully!";
+
+    private final DroneService droneService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerDrone(@RequestBody RegisterRequest registerRequest) {
+        ApiResponse<Drone> apiResponse = new ApiResponse<>(DRONE_REGISTER_SUCCESS,
+                droneService.registerDrone(registerRequest));
+        return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
+    }
+
     // TODO: loading a drone with medication items;
     // TODO: checking loaded medication items for a given drone;
     // TODO: checking available drones for loading;
