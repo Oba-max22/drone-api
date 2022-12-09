aa086934a043f53ab39e4ee7d6c747b7afe039c0
EMMANUEL MACAULAY <emmanuelmcl86@gmail.com>
2022-12-09 22:12:00 +0100

    created getLoadedMedication method

--- src/main/java/com/emmanuel/macaulay/dronesapi/service/DroneService.java
+++ src/main/java/com/emmanuel/macaulay/dronesapi/service/DroneService.java
@@ -1,11 +1,16 @@
 package com.emmanuel.macaulay.dronesapi.service;
 
 import com.emmanuel.macaulay.dronesapi.model.Drone;
+import com.emmanuel.macaulay.dronesapi.model.Medication;
 import com.emmanuel.macaulay.dronesapi.payload.request.RegisterRequest;
+
+import java.util.List;
 
 public interface DroneService {
 
     Drone registerDrone(RegisterRequest registerRequest);
 
     Drone loadDroneWithMedication(String serialNumber, String medicationCode);
+
+    List<Medication> getLoadedMedication(String serialNumber);
 }
