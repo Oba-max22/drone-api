4032cbc2debb24c6c00c3c422c1d1fc3b27108e1
EMMANUEL MACAULAY <emmanuelmcl86@gmail.com>
2022-12-09 12:52:23 +0100

    updated GlobalExceptionHandler

--- src/main/java/com/emmanuel/macaulay/dronesapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/emmanuel/macaulay/dronesapi/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -13,5 +16,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({ConversionFailedException.class, IllegalArgumentException.class})
     public ResponseEntity<String> handleConflict(RuntimeException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorMessage errorMessage = new ErrorMessage(new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(LowBatteryException.class)
+    public ResponseEntity<ErrorMessage> handleLowBatteryException(LowBatteryException exception, WebRequest webRequest) {
+        ErrorMessage errorMessage = new ErrorMessage(new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnavailableForLoadingException.class)
+    public ResponseEntity<ErrorMessage> handleUnavailableForLoadingException(UnavailableForLoadingException exception, WebRequest webRequest) {
+        ErrorMessage errorMessage = new ErrorMessage(new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
